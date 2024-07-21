095c4899d87ea12262511a369622c9a5b66c206a
unknown <siniwyh@icloud.com>
2024-07-21 22:55:50 +0700

    date generating is added

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -2,23 +2,30 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.lang.String.format;
+import static java.time.LocalTime.now;
 
 class CardDeliveryTest {
+    String planDate = LocalDate.now().plusDays(2).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     @Test
     void shouldSendForm() {
+
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
-        $(".calendar-input__custom-control input").setValue("25072024");
+        $(".calendar-input__custom-control input").doubleClick().sendKeys(planDate);
+       // $(".calendar-input__custom-control input").setValue("25072024");
         $("[data-test-id=name] input").setValue("Биба Бобов");
         $("[name='phone']").setValue("+79998887766");
         $(".checkbox__text").click();
         $(".button__content").click();
-        $(".notification_visible").shouldBe(exist, Duration.ofSeconds(20));
+        $(".notification_visible").shouldBe(exist, Duration.ofSeconds(15));
     }
 
 
